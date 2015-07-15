6db90fce2f520d994d9510e957d99d3a9957c284
Arnon Eilat <Arnon.Eilat@gmail.com>
2015-07-15 14:17:34 +0300

    Add Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    clean: {
+      build: {
+        src: ["documentation"]
+      }
+    },
+    jsdoc: {
+      dist: {
+        src: ['modules/*'],
+        options: {
+          destination: 'documentation'
+        }
+      }
+    },
+    jshint: {
+      all: ['Gruntfile.js', 'modules/**/*.js'],
+      options: {
+        reporter: require('jshint-stylish')
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint']);
+
+  grunt.registerTask('doc', ['clean','jsdoc']);
+};
