5dc867576307a0ee61aa19b0a4a528b8903659e7
Arnon Eilat <Arnon.Eilat@gmail.com>
2015-07-15 14:34:00 +0300

    Add modules/fileDb.js

---
+++ modules/fileDb.js
@@ -0,0 +1,121 @@
+"use strict";
+var fs = require('fs');
+var debug = require('debug')('fileDb');
+/**
+ * @description Micro Database file - very simple storage.
+ * @requires fs
+ * @module modules/users/fileDb
+ */
+
+/**
+ * @private
+ * @type {string}
+ **/
+var filePath = './users.db';
+/**
+ * @private
+ * @type {array<object>}
+ **/
+var data = [];
+
+/**
+ * @description Return the highest id on the database
+ * @method
+ * @return {number}
+ */
+module.exports.highestId = function() {
+  var id = 0;
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].id > id) {
+      id = data[i].id;
+    }
+  }
+  return id;
+}
+
+/**
+ * @description Find user by id
+ * @param {number} id - id to find by
+ * @param {userCallback} fn - A callback to run.
+ * @method
+ **/
+module.exports.findById = function(id, fn) {
+    // var idx = id - 1; if (users[idx]) {fn(null, users[idx]); } else {fn(new Error('User ' + id + ' does not exist')); }
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].id === id) {
+        return fn(null, data[i]);
+      }
+    }
+    debugger;
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+  /**
+   * @description Callback with user object
+   * @callback userCallback
+   * @param {object|null} error - Something other than null means you have an error
+   * @param {object} user - User object
+   */
+
+
+
+/**
+ * @description Find user by username
+ * @param {string} username - username to find by
+ * @param {userCallback} fn - A callback to run.
+ * @method
+ **/
+module.exports.findByUsername = function(username, fn) {
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].username === username) {
+      return fn(null, data[i]);
+    }
+  }
+  return fn(null, null);
+}
+
+
+
+
+
+module.exports.add = function(obj) {
+  data.push(obj);
+  return data.length;
+}
+
+module.exports.del = function(num) {
+  data.splice(num, 1);
+  return data.length;
+}
+
+
+module.exports.save = function() {
+  flush();
+}
+
+/**
+ * @private
+ */
+function flush() {
+  fs.writeFileSync(filePath, JSON.stringify(data));
+}
+
+/**
+ * @private
+ */
+function load() {
+  data = JSON.parse(fs.readFileSync(filePath).toString());
+}
+
+
+process.on('exit', function() {
+  flush();
+});
+
+
+if (fs.existsSync(filePath)) { // Load if the file is there
+  debug('Loading ' + filePath);
+  load();
+} else { // Otherwise, create it.
+  debug(filePath + ' Not exists - create it');
+  flush();
+}
