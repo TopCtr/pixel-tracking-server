3cbf036ce05b8f2c609565dfcf209ec565e47be5
Arnon Eilat <Arnon.Eilat@gmail.com>
2015-07-16 14:38:33 +0300

    Remove toJSON from UserFinder

--- modules/model.js
+++ modules/model.js
@@ -1,19 +1,19 @@
 'use strict';
 var fileDb = require('./fileDb');
 var User = require('./User');
+/**
+ * @description Module to manage users within file base database.
+ * @constructor
+ * @requires User
+ * @requires fileDb
+ */
+
 
 module.exports.UserFinder = function(username) {
   var user = fileDb.findByUsername(username);
 
   this.then = function(cb) {
     cb(user);
-  };
-
-  this.toJSON = function() {
-    return JSON.stringify({
-      username: username,
-      password: 'a'
-    });
   };
 };
 
@@ -25,8 +25,8 @@ module.exports.UserAdder = function(user) {
   var u = new User.User(highestId + 1, user.username, user.password, user.firstName, user.lastName);
   fileDb.add(u);
   return {
-    then: function (cb) {
-        cb(u);
+    then: function(cb) {
+      cb(u);
     }
   };
 };
